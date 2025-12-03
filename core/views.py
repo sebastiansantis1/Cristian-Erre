04466bbbb36614fa1ced111fa9ae1698d0f11c82
sebastian <santis.ignasebastian@gmail.com>
2025-12-03 18:41:01 -0300

    cambios en home y base del core

--- core/views.py
+++ core/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
 from cms.models import HomePageContent
+from obras.models import Obra
 
 
 def home(request):
     contenido_home = HomePageContent.get_solo()
-    return render(request, "core/home.html", {"contenido_home": contenido_home})
+
+    # Obtener obras destacadas publicadas y en stock
+    obras_destacadas = Obra.objects.filter(
+        estado="publicada",
+        destacada=True,
+        en_stock=True
+    ).order_by('orden')[:6]  # limite para el carrusel
+
+    context = {
+        "contenido_home": contenido_home,
+        "obras_destacadas": obras_destacadas,
+    }
+
+    return render(request, "core/home.html", context)
